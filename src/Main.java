dc0a9ecbef579d6a6d6e4fc651b6ed070733d76e
D3ze3 <currysplash3@gmail.com>
2025-03-03 18:24:10 -0800

    Init commit

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-public class Main {
+class Main {
     public static void main(String[] args) {
         // Task 1: Count up by 1 from 0 to 30
         System.out.print("Task 1: ");
@@ -27,5 +27,32 @@ public class Main {
             System.out.print(i + " ");
         }
         System.out.println();
+        // Task 5: Right-angled Triangle
+        System.out.println("Task 5:");
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        // Task 6: Inverted Triangle
+        System.out.println("\nTask 6:");
+        for (int i = 5; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        // Task 7: Square Pattern
+        System.out.println("\nTask 7:");
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 5; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
     }
-}
+    }
+
